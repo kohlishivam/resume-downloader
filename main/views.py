e58516d7af02e70ea28e8fac2b2e8ccec637ba4f
Shivam Kohli <kohlishivam5522@gmail.com>
2016-10-24 17:03:20 +0530

    debugging

--- main/views.py
+++ main/views.py
@@ -18,13 +18,13 @@ from main.models import resume_input
 
 PAGE_ACCESS_TOKEN = 'EAATyjn0ZCjToBAI8vGomXbBh1Uk2kHH37E62fjAkcuxhH2bW4rBZCKHftgrIiS72DILFlQVUlk6FO4Ut6k1zquTXnaZCkMLhYf2K6E7ZBt3wLHQilZCZBMfRsV3fQCilng7jfeMRoilcKsywlwnXemRbvF8KKf5kPAvR1BYPLiQwZDZD'
 
-def resume(request,search_string):
+def resume(request):
     # Create the HttpResponse object with the appropriate PDF headers.
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="mycv.pdf"'
     # Create the PDF object, using the response object as its "file."
     p = canvas.Canvas(response)
-    pp = resume_input.objects.get_or_create(fbid=search_string)[0]
+    pp = resume_input.objects.get_or_create(fbid='1204954086214698')[0]
     #print dir(p)
     # Draw things on the PDF. Here's where the PDF generation happens.
     # See the ReportLab documentation for the full list of functionality.
